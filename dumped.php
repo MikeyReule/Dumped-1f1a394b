81a96f9cbe62beda3dadc4b0c0e9ede84e9f09f1
Mike <2114794@talnet.nl>
2019-10-16 14:50:24 +0200

    Uitwerking van de exercise

---
+++ dumped.php
@@ -0,0 +1,19 @@
+<?php
+
+var_dump("Ik tel hoeveel karakters je zin is!");
+echo PHP_EOL;
+
+$antwoord = readline("Wil jij wat zeggen?: ");
+$num = 1337;
+
+var_dump($antwoord);
+
+echo PHP_EOL;
+echo PHP_EOL;
+echo "Dit moet voor de opdracht";
+echo PHP_EOL;
+echo PHP_EOL;
+var_dump("Ik ben een string");
+var_dump($num);
+
+?>	
